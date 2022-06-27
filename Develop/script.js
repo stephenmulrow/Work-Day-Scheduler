8404a423dc1107f9bfc2d0126d257b847495d2e7
smulrow <smulrow@gmail.com>
2022-06-27 13:26:55 -0700

    updating changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,21 @@
-var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+ var today = moment();
+ $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+ var timeOfDay = ["09" , "10", "11", "12" , "1", "2", "3", "4", "5"]
+
+
+ var displayedTime = $("data-time")
+
+ var currentTime = moment().format('h')
+ 
+ for (var i = 0; i < timeOfDay.length; i++){
+   var currentHour = timeOfDay[i]
+ }
+
+ if (timeOfDay[i] < currentTime){
+   $(".desccription").addClass("present")
+ }
+ 
+
 
